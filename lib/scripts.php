da68bbf931c7b0970cbfd4250b522417b6a7eb18
Leo Dion <leogdion@brightdigit.com>
2014-01-25 16:29:41 -0500

    working on static heights

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'abc34c4752f5b59fccfbba2c50ff9cfc');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '2fc12f825500719d16aa5fd5e2ef1d91');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
