a6ba6e9550fab119bfa0e69338b127729d933e32
leogdion <leogdion@brightdigit.com>
2014-02-06 07:57:25 -0500

    Added security fix for login failures

--- lib/custom.php
+++ lib/custom.php
@@ -2,3 +2,5 @@
 /**
  * Custom functions
  */
+function explain_less_login_issues(){ return '<strong>ERROR</strong>: Entered credentials are incorrect.';}
+add_filter( 'login_errors', 'explain_less_login_issues' );
