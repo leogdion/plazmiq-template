4b9e079277f1678e1a8463e3708f0d03c2a8e810
leogdion <leogdion@brightdigit.com>
2014-03-25 18:03:54 -0400

    Fixed footer links

--- templates/footer.php
+++ templates/footer.php
@@ -15,7 +15,7 @@
       <h4>BrightDigit</h4>
       <ul>
         <li><a href="/blog" target="_blank" title="blog"><img src="/assets/img/wordpress-logo.svg" height="15" style="margin-bottom: 4px;"></a></li>
-        <li><a href="//www.facebook.com/pages/Bright-Digit" target="_blank" title="facebook"><i class="fa fa-facebook"></i></a></li>
+        <li><a href="//www.facebook.com/BrightDigit" target="_blank" title="facebook"><i class="fa fa-facebook"></i></a></li>
         <li><a href="//www.twitter.com/brightdigit" target="_blank" title="twitter"><i class="fa fa-twitter"></i></a></li>
         <li><a href="//www.google.com/+BrightdigitLLC" target="_blank" rel="publisher" title="google+"><i class="fa fa-google-plus"></i></a></li>
         <li><a href="//www.linkedin.com/company/bright-digit" target="_blank" title="linkedin"><i class="fa fa-linkedin"></i></a></li>
@@ -25,8 +25,8 @@
       <h4>Leo G Dion</h4>
       <ul>
         <li><a href="//leogdion.name" target="_blank" title="blog"><img src="/assets/img/wordpress-logo.svg" height="15" style="margin-bottom: 4px;"></a></li>
-        <li><a href="//www.facebook.com/pages/Bright-Digit" target="_blank" title="facebook"><i class="fa fa-facebook"></i></a></li>
-        <li><a href="//www.twitter.com/brightdigit" target="_blank" title="twitter"><i class="fa fa-twitter"></i></a></li>
+        <li><a href="//http://facebook.com/leogdion" target="_blank" title="facebook"><i class="fa fa-facebook"></i></a></li>
+        <li><a href="//www.twitter.com/leogdion" target="_blank" title="twitter"><i class="fa fa-twitter"></i></a></li>
         <li><a href="//plus.google.com/115750410676034044633" target="_blank" title="google+"><i class="fa fa-google-plus"></i></a></li>
         <li><a href="//www.linkedin.com/in/leogdion/" target="_blank" title="linkedin"><i class="fa fa-linkedin"></i></a></li>
         <li><a href="//github.com/leogdion" target="_blank" title="github"><i class="fa fa-github"></i></a></li>
