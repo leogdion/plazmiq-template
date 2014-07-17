001ec86cae3437c50041be6b7ab206f1c3c35f22
Leo Dion <leogdion@brightdigit.com>
2014-07-17 18:43:47 -0400

    working on css transitions

--- static/js/controllers/login.js
+++ static/js/controllers/login.js
@@ -1,10 +1,19 @@
-define(function () {
+define(['zepto'], function ($) {
+  function validate() {
+    $('button').not('.inactive').prop('disabled', true);
+
+  }
   return {
     template: 'login',
     events: {
       'button.inactive': {
         'click': function (e) {
           console.log('switch');
+          $('form h1 span').toggleClass('inactive');
+          $('button').toggleClass('inactive').prop('disabled', false);
+          validate();
+          $('#registration').toggleClass('collapse', this.getAttribute('id') !== 'register');
+          //$('button.inactive').prop('disabled', false);
           return false;
         }
       },
