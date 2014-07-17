ce922f403401dcfd98d134734e2c75320a19e324
Leo Dion <leogdion@brightdigit.com>
2014-07-17 12:45:45 -0400

    added first route

--- static/js/app.js
+++ static/js/app.js
@@ -1,6 +1,21 @@
-define(['templates', 'zepto'], function (templates, $) {
+define(['templates', 'zepto', 'crossroads', 'hasher', './controllers/index'], function (templates, $, crossroads, hasher, controllers) {
   var app = {};
-  $('main').html(templates.home());
+  //setup crossroads
+  crossroads.addRoute('/', function () {
+    $('main').html(templates[controllers.home.template]());
+  });
+  //crossroads.addRoute('lorem/ipsum');
+  crossroads.routed.add(console.log, console); //log all routes
+  //setup hasher
 
+  function parseHash(newHash, oldHash) {
+    crossroads.parse(newHash);
+  }
+  hasher.initialized.add(parseHash); //parse initial hash
+  hasher.changed.add(parseHash); //parse hash changes
+  hasher.init(); //start listening for history change
+  //update URL fragment generating new history record
+  //hasher.setHash('lorem/ipsum');
+  //$('main').html(templates.home());
   return app;
 });
---
+++ static/js/controllers/index.js
@@ -0,0 +1,5 @@
+define(['./home'], function (home) {
+  return {
+    home: home
+  };
+});
