2717b15ada1cf63c8c63dae9ea868487b2481c05
Leo Dion <leogdion@brightdigit.com>
2014-12-29 15:54:05 -0500

    working on posts and templates

---
+++ index.js
@@ -0,0 +1,41 @@
+if (!global.Intl) {
+    global.Intl = require('intl');
+}
+
+var Metalsmith = require('metalsmith'),
+    markdown   = require('metalsmith-markdown'),
+    templates  = require('metalsmith-templates'),
+    Handlebars = require('handlebars'),
+    fs         = require('fs'),
+    async      = require('async'),
+    path       = require('path'),
+    glob       = require('glob'),
+    HandlebarsIntl = require('handlebars-intl');
+
+HandlebarsIntl.registerWith(Handlebars);
+//async.each(fs.readDir('./static/templates/partials'))
+
+glob("./static/templates/partials/*.html", function (er, files) {
+  async.each(files, function (file, cb) {
+    fs.readFile(file, function (error, content) {
+      console.log(path.basename(file, '.html'));
+      Handlebars.registerPartial(path.basename(file, '.html'), content.toString());
+      cb(error);
+    });
+  }, function (error) {
+    console.log(error);
+    Metalsmith(__dirname)
+        .source('static')
+        .use(markdown())
+        .use(templates({engine : 'handlebars', directory: 'static/templates'}))
+        .destination('./public')
+        .build(function (error, files) {
+          console.log(error);
+          console.log(files);
+        });
+  });
+    // files is an array of filenames.
+  // If the `nonull` option is set, and nothing
+  // was found, then files is ["**/*.js"]
+  // er is an error object or null.
+});
