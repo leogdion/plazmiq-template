ef4d05e41f02bebe79f53b8f98cf45ffd12123c5
Leo Dion <leogdion@brightdigit.com>
2015-01-07 17:04:04 -0500

    formatted gulpfile, fixed date on blog post

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,5 @@
-
 if (!global.Intl) {
-    global.Intl = require('intl');
+  global.Intl = require('intl');
 }
 
 var gulp = require('gulp'),
@@ -11,75 +10,59 @@ var gulp = require('gulp'),
     es = require('event-stream'),
     async = require('async'),
     rimraf = require('rimraf'),
-    rename = require('gulp-rename')
-    browserify = require('browserify');
+    rename = require('gulp-rename'),
+    browserify = require('browserify'),
     transform = require('vinyl-transform'),
-    gulpsmith = require('gulpsmith'), 
+    gulpsmith = require('gulpsmith'),
     Metalsmith = require('metalsmith'),
-    markdown   = require('metalsmith-markdown'),
-    templates  = require('metalsmith-templates'),
-    excerpts  = require('metalsmith-excerpts'),
-    collections  = require('metalsmith-collections'),
+    markdown = require('metalsmith-markdown'),
+    templates = require('metalsmith-templates'),
+    excerpts = require('metalsmith-excerpts'),
+    collections = require('metalsmith-collections'),
     Handlebars = require('handlebars'),
-    fs         = require('fs'),
-    async      = require('async'),
-    path       = require('path'),
-    glob       = require('glob'),
+    fs = require('fs'),
+    async = require('async'),
+    path = require('path'),
+    glob = require('glob'),
     HandlebarsIntl = require('handlebars-intl'),
     gulp_front_matter = require('gulp-front-matter');
-    assign = require('lodash.assign');
+assign = require('lodash.assign');
 
-gulp.task('default', ['clean', 'browserify', 'sass', 'copy', 'metalsmith', 'bump']);
+gulp.task('default', ['clean', 'browserify', 'sass', 'copy', 'lint', 'metalsmith', 'bump']);
 
 gulp.task('clean', function (cb) {
   async.each(['public', '.tmp', '.coverdata'], rimraf, cb);
 });
 
 gulp.task('metalsmith', ['clean'], function (cb) {
-
-
-HandlebarsIntl.registerWith(Handlebars);
-glob("./static/templates/partials/*.html", function (er, files) {
-  async.each(files, function (file, asynccb) {
-    fs.readFile(file, function (error, content) {
-      Handlebars.registerPartial(path.basename(file, '.html'), content.toString());
-      asynccb(error);
+  HandlebarsIntl.registerWith(Handlebars);
+  glob("./static/templates/partials/*.html", function (er, files) {
+    async.each(files, function (file, asynccb) {
+      fs.readFile(file, function (error, content) {
+        Handlebars.registerPartial(path.basename(file, '.html'), content.toString());
+        asynccb(error);
+      });
+    }, function (error) {
+      Handlebars.registerHelper('safe', function (contents) {
+        return new Handlebars.SafeString(contents);
+      });
+      gulp.src('./static/html/**/*').pipe(gulp_front_matter()).on("data", function (file) {
+        assign(file, file.frontMatter);
+        delete file.frontMatter;
+      }).pipe(
+      gulpsmith().use(collections({
+        posts: {
+          pattern: 'posts/*.md',
+          sortBy: 'date',
+          reverse: true
+        }
+      })).use(markdown()).use(excerpts()).use(templates({
+        engine: 'handlebars',
+        directory: 'static/templates'
+      }))).pipe(gulp.dest("./public"));
     });
-  }, function (error) {
-    Handlebars.registerHelper('safe', function(contents) {
-      return new Handlebars.SafeString(contents);
-    });
-    gulp.src('./static/html/**/*').pipe(gulp_front_matter()).on("data", function(file) {
-    assign(file, file.frontMatter); 
-    delete file.frontMatter;
-}).pipe(
-    //Metalsmith(__dirname)
-      //  .source('static/html')
-      gulpsmith()
-        .use(collections({
-            posts: {
-                pattern: 'posts/*.md',
-                sortBy: 'date',
-                reverse: true
-            }
-        }))
-        .use(markdown())
-        .use(excerpts())
-        .use(templates({engine : 'handlebars', directory: 'static/templates'}))
-        /*.destination('./public')
-        .build(function (error, files) {
-          console.log(error);
-          console.log(files);
-          cb(error);
-        });
-*/
-  ).pipe(gulp.dest("./public"));
-    // files is an array of filenames.
-  // If the `nonull` option is set, and nothing
-  // was found, then files is ["**/*.js"]
-  // er is an error object or null.
+  });
 });
-})});
 
 gulp.task('copy', ['clean'], function () {
   return es.merge(
@@ -91,15 +74,14 @@ gulp.task('sass', ['clean'], function () {
 });
 
 gulp.task('browserify', function () {
-  var browserified = transform(function(filename) {
+  var browserified = transform(function (filename) {
     var b = browserify(filename);
     return b.bundle();
   });
-  
-  return gulp.src(['./static/js/main.js'])
-    .pipe(browserified)
-    //.pipe(uglify())
-    .pipe(gulp.dest('./public/js'));
+
+  return gulp.src(['./static/js/main.js']).pipe(browserified)
+  //.pipe(uglify())
+  .pipe(gulp.dest('./public/js'));
 });
 
 gulp.task('bump', function () {
@@ -112,7 +94,7 @@ gulp.task('lint', ['beautify'], function () {
   return gulp.src(['./gulpfile.js', 'static/js/**/*.js']).pipe(jshint()).pipe(jshint.reporter('default'));
 });
 
-gulp.task('beautify', ['lint'], function () {
+gulp.task('beautify', function () {
   gulp.src(['./gulpfile.js', 'static/js/**/*.js'], {
     base: '.'
   }).pipe(beautify({
