c9501787563505be7870eebb5b1194ac80ce4fd8
Leo Dion <leogdion@brightdigit.com>
2015-01-10 15:49:15 -0500

    fixed production gulp task, added web font

--- static/js/main.js
+++ static/js/main.js
@@ -1,1 +1,15 @@
 var $ = require("browserify-zepto");
+
+WebFontConfig = {
+  google: {
+    families: ['Oxygen+Mono::latin', 'Oxygen:700,400,300:latin', 'Cardo:400,700:latin']
+  }
+};
+(function () {
+  var wf = document.createElement('script');
+  wf.src = ('https:' == document.location.protocol ? 'https' : 'http') + '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+  wf.type = 'text/javascript';
+  wf.async = 'true';
+  var s = document.getElementsByTagName('script')[0];
+  s.parentNode.insertBefore(wf, s);
+})();
