3fcbb7182be738feb79160742293f4497bf85f44
Leo G Dion <leogdion@brightdigit.com>
2015-07-13 13:24:47 -0400

    creating session on authentication

--- app/index.js
+++ app/index.js
@@ -2,7 +2,8 @@ var express = require("express");
 var app = express(),
     bodyParser = require('body-parser'),
     cookieParser = require('cookie-parser'),
-    session = require('express-session');
+    session = require('express-session'),
+    BearerStrategy = require('passport-http-bearer').Strategy;
 
 var Sequelize = require('sequelize');
 var sequelize = new Sequelize('auth-sample', 'auth-sample', null, {dialect: 'postgres'});
@@ -10,6 +11,17 @@ var passport = require('passport');
 var passportLocalSequelize = require('passport-local-sequelize');
 
 var User = passportLocalSequelize.defineUser(sequelize);
+
+var Session = sequelize.define('Session', {
+  id: {
+    type : Sequelize.UUID,
+    unique: true,
+    allowNull: false,
+    defaultValue: Sequelize.UUIDV4
+  }
+})
+
+Session.belongsTo(User);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -41,6 +53,17 @@ app.post('/users',function(req, res, next) {
     console.log('user registered!');
 
     res.redirect('/');
+  });
+});
+
+app.post('/sessions', function (req, res, next) {
+  User.authenticate()(req.body.username, req.body.password, function (_, user, error) {
+    console.log(arguments);
+    Session.create({'UserId' : user.id}).then(
+      function (session) {
+        res.send(session);
+      }
+    );
   });
 });
 
