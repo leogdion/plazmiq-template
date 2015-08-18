84f25a90ed44522a8b840a75fce38ff1c2fe6c2b
Leo Dion <leogdion@brightdigit.com>
2015-08-17 20:19:51 -0400

    adding models and sequelize

--- app/index.js
+++ app/index.js
@@ -1,8 +1,15 @@
 var express = require('express');
 var app = express();
 var roust = require('roust');
+var Sequelize = require('Sequelize');
 
 roust(app, '/api/v1', [__dirname + '/controllers']);
+
+/*
+var sequelize = new Sequelize('beginkit-master', 'beginkit-user', '', {
+  dialect: 'postgres'
+});
+*/
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
