9cb4cba74e04e45d9bf94f26c5fb89ab2dab1257
Leo Dion <leogdion@brightdigit.com>
2015-08-18 14:41:21 -0400

    added build steps

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,30 @@
 var gulp = require('gulp');
 var beautify = require('gulp-beautify');
+var jshint = require('gulp-jshint');
+var bump = require('gulp-bump');
+
+gulp.task('lint', ['beautify'], function () {
+  return gulp.src(['./*.js', 'static/js/**/*.js']).pipe(jshint()).pipe(jshint.reporter('default'));
+});
+gulp.task('bump', [], function () {
+  return gulp.src(['./package.json']).pipe(bump({
+    type: 'patch'
+  })).pipe(gulp.dest('./'));
+});
 
 gulp.task('beautify', function () {
-  gulp.src(['./app/**/*.js', 'gulpfile.js'], {
+  return gulp.src(['./app/**/*.js', 'gulpfile.js'], {
     base: '.'
   }).pipe(beautify({
     indentSize: 2
-  })).pipe(gulp.dest('.'))
+  })).pipe(gulp.dest('.'));
 });
 gulp.task('heroku:production', ['submodules']);
 gulp.task('test', function () {
   // place code for your default task here
 });
 
-gulp.task('default', ['submodules', 'beautify']);
+gulp.task('default', ['submodules', 'lint', 'bump']);
 
 gulp.task('submodules', function () {
   return gulp.src('modules/**/*').pipe(gulp.dest('node_modules'));
