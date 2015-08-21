6191f97a8aacd9280fc06cb76f76ff0b53cc8a66
Leo Dion <leogdion@brightdigit.com>
2015-08-21 09:14:30 -0400

    using js to load css

--- static/js/app.js
+++ static/js/app.js
@@ -1,5 +1,15 @@
 module.exports = {
   start: function () {
     console.log("Hello World!");
+    var cb = function () {
+      var l = document.createElement('link');
+      l.rel = 'stylesheet';
+      l.href = '/css/style.css';
+      var h = document.getElementsByTagName('head')[0];
+      h.parentNode.insertBefore(l, h);
+    };
+    var raf = requestAnimationFrame || mozRequestAnimationFrame || webkitRequestAnimationFrame || msRequestAnimationFrame;
+    if (raf) raf(cb);
+    else window.addEventListener('load', cb);
   }
 };
