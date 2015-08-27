0cc82d6c97ceeb598632a2b14744dbe09cc1463e
Leo Dion <leogdion@brightdigit.com>
2015-08-27 12:28:18 -0400

    working on email templates

--- app/controllers/user.js
+++ app/controllers/user.js
@@ -1,4 +1,14 @@
 var configuration = require('../libs/configuration');
+var url = require('url');
+var postmark = require('postmark');
+var pmConfig = require('../../gulp/revquire')({
+  "apiKey": "POSTMARK_API_KEY",
+  "apiToken": "POSTMARK_API_TOKEN",
+  "inboundAddress": "POSTMARK_INBOUND_ADDRESS",
+  "smtpServe": "POSTMARK_SMTP_SERVER"
+}, __dirname + '/../../.credentials/postmark.json');
+
+var client = new postmark.Client (pmConfig.apiKey);
 
 module.exports = function (include) {
 
@@ -16,7 +26,8 @@ module.exports = function (include) {
           res.send('show');
         },
         create: function (req, res, next) {
-          console.log(req);
+          var components = url.parse(req.get('referer') || req.get('origin'));
+
           User.register({
             name: req.body.name,
             emailAddress: req.body.emailAddress
@@ -24,7 +35,12 @@ module.exports = function (include) {
             if (err) {
               return next(err);
             }
-
+            var fullUrl = url.format({
+              host: components.host,
+              protocol: components.protocol,
+              slashes: components.slashes,
+              search: "activationKey=" + user.activationKey
+            });
             res.send({
               "name": user.name
             });
